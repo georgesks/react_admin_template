e7d7e45a88621d168bc0c64712eee697df28c6e1
Jorge Gomez <george.gmes@gmail.com>
2023-11-24 18:25:45 -0600

    expand or collapse sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@heroicons/react/24/outline/HomeIcon';
 import ShoppingBagIcon from '@heroicons/react/24/outline/ShoppingBagIcon';
@@ -6,8 +6,14 @@ import CogIcon from '@heroicons/react/24/outline/CogIcon';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   return (
-    <div className="flex flex-col w-64 h-full bg-gray-800 text-white px-4">
+    <div
+      className={`flex flex-col ${isCollapsed ? 'w-16' : 'w-64'} h-full bg-gray-800 text-white px-4 transition-width duration-300`}
+      onMouseEnter={() => setIsCollapsed(false)}
+      onMouseLeave={() => setIsCollapsed(true)}
+    >
       <nav className="mt-5">
         <Link to="/page1" className="flex items-center space-x-2 sidebar-link">
           <HomeIcon className="h-5 w-5" />
@@ -20,8 +26,7 @@ const Sidebar = () => {
         <Link to="/page3" className="flex items-center space-x-2 sidebar-link">
           <CogIcon className="h-5 w-5" />
           <span>Page 3</span>
-        </Link>
-        {/* Continúa agregando más enlaces con la misma estructura si es necesario */}
+        </Link>        
       </nav>
     </div>
   );
